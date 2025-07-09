fa79e97c9eeb1d19740b8213631de139ac2e8745
harshith1511 <harshithrajpalegar@gmail.com>
2025-07-09 16:00:29 +0530

    Updated code5.py

--- code5.py
+++ code5.py
@@ -1,6 +1,7 @@
 from functionsOfcode5 import average, gradeChecker
-
+name=input("Enter your name:")
 marks=list(map(int,input("Enter marks of student:").split()))
 avg=average(marks)
+print("Marks:",marks)
 print("the avg is",avg)
 print("the grade is",gradeChecker(avg))
